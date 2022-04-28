cc0c94263d43e552feeb1a96994959105e40eb20
nowislewis <1226228799@qq.com>
2022-04-29 01:01:59 +0800

    add line number here

--- nowisemacs-base.el
+++ nowisemacs-base.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t; -*-
+;; basic modes
 (setq use-short-answers t)
 (setq confirm-kill-emacs 'y-or-n-p)
 (setq ring-bell-function 'ignore)       ;关闭烦人的出错时的提示声
@@ -18,6 +19,9 @@
 ;; (setq frame-resize-pixelwise t) ;设置缩放的模式,避免Mac平台最大化窗口以后右边和下边有空隙
 ;; (set-frame-parameter nil 'fullscreen 'maximized)
 
+(savehist-mode)
+
+;; setup
 (require 'setup)
 
 (setup-define :delay
@@ -52,5 +56,107 @@
           (feature-string (symbol-name (setup-get 'feature))))
       `(autoload ',load-func ,feature-string nil t)))
   :documentation "Load the current feature after FEATURES.")
+
+;; insert pairs
+  (defun insert-quotations (&optional arg)
+    "Enclose following ARG sexps in quotation marks.
+    Leave point after open-paren."
+    (interactive "*P")
+    (insert-pair arg ?\' ?\'))
+
+  (defun insert-quotes (&optional arg)
+    "Enclose following ARG sexps in quotes.
+    Leave point after open-quote."
+    (interactive "*P")
+    (insert-pair arg ?\" ?\"))
+
+  (defun insert-backquote (&optional arg)
+    "Enclose following ARG sexps in quotations with backquote.
+    Leave point after open-quotation."
+    (interactive "*P")
+    (insert-pair arg ?\` ?\'))
+
+  (defun insert-star (&optional arg)
+    "Enclose following ARG sexps in stars.
+  Leave point after open-quotation."
+    (interactive "*P")
+    (insert-pair arg ?\* ?\*))
+
+  (defun insert-bracket (&optional arg)
+    "Enclose following ARG sexps in brackets.
+  Leave point after open-quotation."
+    (interactive "*P")
+    (insert-pair arg ?\[ ?\]))
+
+  (defun insert-curly (&optional arg)
+    "Enclose following ARG sexps in curly braces.
+  Leave point after open-quotation."
+    (interactive "*P")
+    (insert-pair arg ?\{ ?\}))
+
+  (defun insert-equate (&optional arg)
+    "Enclose following ARG sexps in equations.
+  Leave point after open-quotation."
+    (interactive "*P")
+    (insert-pair arg ?\= ?\=))
+
+;; line number
+;; only enable line number in some modes, borrowed from lazycat-emacs
+  (setq display-line-numbers-width-start t)
+  (setq line-number-display-limit large-file-warning-threshold)
+  (setq line-number-display-limit-width 1000)
+
+  (dolist (hook (list
+                 'c-mode-common-hook
+                 'c-mode-hook
+                 'emacs-lisp-mode-hook
+                 'lisp-interaction-mode-hook
+                 'lisp-mode-hook
+                 'java-mode-hook
+                 'asm-mode-hook
+
+                 'haskell-mode-hook
+                 'rcirc-mode-hook
+                 'erc-mode-hook
+                 'sh-mode-hook
+                 'makefile-gmake-mode-hook
+                 'python-mode-hook
+                 'js-mode-hook
+                 'html-mode-hook
+                 'css-mode-hook
+                 'tuareg-mode-hook
+                 'go-mode-hook
+                 'coffee-mode-hook
+                 'qml-mode-hook
+                 'markdown-mode-hook
+                 'slime-repl-mode-hook
+                 'package-menu-mode-hook
+                 'cmake-mode-hook
+                 'php-mode-hook
+                 'web-mode-hook
+                 'coffee-mode-hook
+                 'sws-mode-hook
+                 'jade-mode-hook
+                 'vala-mode-hook
+                 'rust-mode-hook
+                 'ruby-mode-hook
+                 'qmake-mode-hook
+                 'lua-mode-hook
+                 'swift-mode-hook
+                 'llvm-mode-hook
+                 'conf-toml-mode-hook
+                 'nxml-mode-hook
+                 'nim-mode-hook
+                 'org-mode-hook
+                 'verilog-mode-hook
+                 ))
+    (add-hook hook (lambda () (display-line-numbers-mode))))
+
+;; auto-revert changed files
+
+;;; Revert Dired and other buffers
+(setq global-auto-revert-non-file-buffers t)
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
 
 (provide 'nowisemacs-base)
