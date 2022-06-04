6233adb77c3cc7fa39dc9a2dbdb227cf6f17649a
nowislewis <1226228799@qq.com>
2022-06-04 22:43:42 +0800

    add defer support for idle load func

--- nowisemacs-base.el
+++ nowisemacs-base.el
@@ -35,6 +35,13 @@
                           (lambda () (require ',(setup-get 'feature)))))
   :documentation "Delay loading the feature until a certain amount of idle time has passed.")
 
+(setup-define :defer
+  (lambda (features)
+    `(run-with-idle-timer 1 nil
+                          (lambda ()
+                            ,features)))
+    :documentation "Delay loading the feature until a certain amount of idle time has passed."
+    :repeatable t)
 
 (setup-define :load-after
     (lambda (features &rest body)
